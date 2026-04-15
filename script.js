300a661f95b3362e7229873fcdd638db34beec5f
Orssi Mpiere <orssimpiere5@gmail.com>
2026-04-15 23:05:24 +0100

    finished page

--- script.js
+++
@@ -1,5 +0,0 @@
-import { $ } from "./src/scripts/helper.js";
-import { initializeSearch } from "./src/scripts/search.js";
-
-// Initialiser la fonctionnalité de recherche lorsque le DOM est chargé
-document.addEventListener("DOMContentLoaded", initializeSearch);
